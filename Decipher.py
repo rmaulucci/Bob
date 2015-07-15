46a2a2544e4612048e08ed7a120dd6ac6bae4b52
rmaulucci <rmaulucci11@gmail.com>
2015-07-15 13:17:31 -0400

    Vingere Crack
    Solving for the key

--- Decipher.py
+++ Decipher.py
@@ -2,11 +2,11 @@ code = raw_input('Enter code: ')
 
 def decipher(word):
     new_word = list(word)
-    dict = {'A':'b', 'B':'o', 'C':'s', 'D':'h', 'E':'g', 'F':'v',
-            'G':'m', 'H':'x', 'I':'k', 'J':'t', 'K':'y', 'L':'p',
-            'M':'u', 'N':'r', 'O':'j', 'P':'z', 'Q':'a', 'R':'q',
-            'S':'e', 'T':'w', 'U':'i', 'V':'l', 'W':'n', 'X':'f',
-            'Y':'c', 'Z':'d', ' ':''}
+    dict = {'A':'b', 'B':'o', 'C':'i', 'D':'h', 'E':'g', 'F':'k',
+            'G':'n', 'H':'q', 'I':'v', 'J':'t', 'K':'w', 'L':'y',
+            'M':'u', 'N':'r', 'O':'x', 'P':'z', 'Q':'a', 'R':'j',
+            'S':'e', 'T':'m', 'U':'s', 'V':'l', 'W':'d', 'X':'f',
+            'Y':'p', 'Z':'c', ' ':''}
     for i in range(len(new_word)):
         new_word[i] = dict[new_word[i]]
     new_word = ''.join(new_word)
